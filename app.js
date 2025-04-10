0b7e3f131c05e8919acf7c9a33a98b46d95f696c
Stephinefnzx <stephinezelzf@gmx.com>
2025-04-10 03:38:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,3 @@ app.post('/check-mail', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
